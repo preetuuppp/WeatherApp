f384a742860e12d0aaa3d6469e923a671a1af578
suhail khan <preetikushwaha0110@gmail.com>
2024-09-21 22:55:27 +0530

    map fixed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,7 +8,7 @@ const Map = ({ location, zoom = 12 }) => {
   };
 
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+    <LoadScript googleMapsApiKey={'AIzaSyACRnrdDeJ5zs4jIQUAV81ZsVw6C9Wi-oQ'}>
       <GoogleMap mapContainerStyle={mapStyles} center={location} zoom={zoom}>
         <Marker position={location} />
       </GoogleMap>
